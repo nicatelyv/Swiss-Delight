086166d4945a91ee872e09fd21c10217aa6fbaf6
nicatelyv <nikoalyv@gmail.com>
2023-02-10 02:58:04 +0400

    Backend login register added

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+dotenv.config()
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.CONNECT_URL)
+.then(()=> console.log("DB Connection Successfull !"))
+.catch((err)=>{
+    console.log(err);
+})
+
+app.use(express.json())
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+app.listen(process.env.PORT || 5000 , ()=>{
+    console.log("Backend server is running !");
+});
---
+++ server/models/Product.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const ProductSchema = new mongoose.Schema({
+    productname: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    desc: { type: String, required: true },
+    img1: { type: String, required: true },
+    img2: { type: String, required: true },
+    img3: { type: String, required: true },
+    img4: { type: String, required: true },
+    sku: { type: Number, required: true },
+    tags: { type: String, required: true },
+    categories: { type: Array },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
