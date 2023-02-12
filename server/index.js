b52ce622fa9b69db645f2736cd8e2bc2c555c280
nicatelyv <nikoalyv@gmail.com>
2023-02-12 15:07:35 +0400

    Backend - order income function was added

--- server/index.js
+++ server/index.js
@@ -5,21 +5,25 @@ const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 dotenv.config()
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.CONNECT_URL)
-.then(()=> console.log("DB Connection Successfull !"))
-.catch((err)=>{
-    console.log(err);
-})
+    .then(() => console.log("DB Connection Successfull !"))
+    .catch((err) => {
+        console.log(err);
+    })
 
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
-app.listen(process.env.PORT || 5555 , ()=>{
+app.listen(process.env.PORT || 5555, () => {
     console.log("Backend server is running !");
 });
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,5 +1,5 @@
 const { json } = require("express");
-const Order = require("../models/Product");
+const Order = require("../models/Order");
 const { verifyTokenAndAuthorization, verifyTokenAndAdmin, verifyToken } = require("./verifyToken");
 
 const router = require("express").Router();
@@ -65,5 +65,33 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
+
+//Get monthly income
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1))
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1))
+
+    try {
+        const income = await Order.aggregate([
+            { $match: { createdAt: { $gte: previousMonth } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                    sales: "$amount",
+                }
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: "$sales" },
+                },
+            },
+        ]);
+        res.status(200).json(income);
+    } catch {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
