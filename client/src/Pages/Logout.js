794b2599cea9565adf58ed2777227cf39469ef2e
nicatelyv <nikoalyv@gmail.com>
2024-05-07 17:23:33 +0400

    logout update

--- client/src/Pages/Logout.js
+++ client/src/Pages/Logout.js
@@ -4,9 +4,12 @@ import { useNavigate } from 'react-router-dom'
 export default function Index() {
 
   let navigate = useNavigate()
+  const keysToRemove = ['username', 'firstName', 'lastName', 'email', 'token', 'isAdmin']
 
   function logout() {
-    localStorage.clear()
+    keysToRemove.forEach(function (key) {
+      localStorage.removeItem(key);
+    });
     navigate("/")
     window.location.reload(false);
   }
