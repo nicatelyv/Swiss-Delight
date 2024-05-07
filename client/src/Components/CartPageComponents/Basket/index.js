3229c45b3f4a66fc48290a4f8206bebfa364d0dd
nicatelyv <nikoalyv@gmail.com>
2024-05-07 14:13:53 +0400

    Basket update

--- client/src/Components/CartPageComponents/Basket/index.js
+++ client/src/Components/CartPageComponents/Basket/index.js
@@ -15,27 +15,28 @@ function Basket() {
     return (
         <section className='cartSection'>
             <div className='cartSectionMain'>
-                {cart.products.map((product, index) => (
-                    <div key={index} className='Cart'>
-                        <img src={product.img1} alt='foto' />
-                        <div className='cartItemTexts'>
-                            <h2 id='cartItemH2'>{product.productname}</h2>
-                            <p id='cartItemP'>{product.tags}</p>
-                            <p id='cartItemP'>${product.price}.00</p>
-                        </div>
-                        <div>
-                            <div id='cartItemCount'>
-                                <h4>{product.quantity}x</h4>
+                {cart.total !== 0 ?
+                    (cart.products.map((product, index) => (
+                        <div key={index} className='Cart'>
+                            <img src={product.img1} alt='foto' />
+                            <div className='cartItemTexts'>
+                                <h2 id='cartItemH2'>{product.productname}</h2>
+                                <p id='cartItemP'>{product.tags}</p>
+                                <p id='cartItemP'>${product.price}.00</p>
                             </div>
-                            <h3 id='cartItemH3'>${product.price * product.quantity}.00</h3>
+                            <div>
+                                <div id='cartItemCount'>
+                                    <h4>{product.quantity}x</h4>
+                                </div>
+                                <h3 id='cartItemH3'>${product.price * product.quantity}.00</h3>
+                            </div>
                         </div>
-                    </div>
-                ))
+                    ))) : <h2>Sepet boş</h2>
                 }
-                <h2 id='cartTotalPrice'>Toplam: ${cart.total}.00</h2>
-                {localStorage.getItem('username') ?
+                {cart.total !== 0 && <h2 id='cartTotalPrice'>Toplam: ${cart.total}.00</h2>}
+                {cart.total !== 0 ? localStorage.getItem('username') ?
                     <button onClick={handleClick} style={{ cursor: "pointer", background: "none", border: "2px solid gold", width: "150px", height: "40px", borderRadius: "10px", fontSize: "20px" }}>Onayla</button>
-                    : <Link to={'/login'}><button style={{ cursor: "pointer", background: "none", border: "2px solid gold", width: "150px", height: "40px", borderRadius: "10px", fontSize: "20px" }}>Onayla</button></Link>
+                    : <Link to={'/login'}><button style={{ cursor: "pointer", background: "none", border: "2px solid gold", width: "150px", height: "40px", borderRadius: "10px", fontSize: "20px" }}>Onayla</button></Link> : <></>
                 }
             </div>
         </section>
