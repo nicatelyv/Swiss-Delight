e7938362840d7c15aeaf75152c9a9813efad2c94
nicatelyv <nikoalyv@gmail.com>
2024-05-07 11:49:41 +0400

    redux update

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -1,25 +1,55 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('cartState');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('cartState', serializedState);
+    } catch {
+        // 
+    }
+};
 
 const cartSlice = createSlice({
     name: "cart",
-    initialState: {
+    initialState: loadState() || {
         products: [],
         quantity: 0,
         total: 0,
     },
     reducers: {
         addProduct: (state, action) => {
-            state.quantity += 1;
-            state.products.push(action.payload);
+            const existingProduct = state.products.find(product => product._id === action.payload._id);
+
+            if (existingProduct) {
+                existingProduct.quantity += action.payload.quantity;
+            } else {
+                state.products.push(action.payload);
+            }
+            state.quantity += action.payload.quantity;
             state.total += action.payload.price * action.payload.quantity;
+            saveState(state);
         },
-        clearBasket : (state,action) => {
-            state.quantity = 0
-            state.products = []
-            state.total = 0
+        clearBasket: (state, action) => {
+            state.quantity = 0;
+            state.products = [];
+            state.total = 0;
+
+            saveState(state);
         }
     },
 });
 
-export const { addProduct , clearBasket } = cartSlice.actions
+export const { addProduct, clearBasket } = cartSlice.actions;
 export default cartSlice.reducer;
